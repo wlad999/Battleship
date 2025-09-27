2e70ad7cd0cdef929763065011e340606788c8ee
wlad999 <wlad2588@gmail.com>
2025-09-27 20:54:05 +0300

    HitWaveSVG fixed on the mobile width

--- src/app/components/hitWave/index.js
+++ src/app/components/hitWave/index.js
@@ -15,8 +15,10 @@ export default function HitWaveSVG({ lastHitId = null, isPlayer = false }) {
         position: "absolute",
         top: 0,
         left: 0,
-        width: "100vw",
-        height: "100vh",
+        right: 0,
+        bottom: 0,
+        width: "100%",
+        height: "100%",
         pointerEvents: "none",
         zIndex: 1,
       }}
